8154e45cbad3af9793958c338b9714ffd70331b9
CodedByWaheed <waheed20032018@gmail.com>
2026-06-20 15:08:36 +0300

    Update Azure deployment profile and DB connection strings
    Added Azure Web Deploy publish profile for BreadApp. Updated appsettings.json and clsConnectionSetting.cs to use Azure SQL Database connection string instead of local SQL Server.

--- BreadApp_DL/clsConnectionSetting.cs
+++ BreadApp_DL/clsConnectionSetting.cs
@@ -8,7 +8,7 @@ namespace BreadApp_DL
 {
     public class clsConnectionSetting
     {
-        public static string ConnectionString = "Server=localhost;Database=BreadApp;User Id=sa;Password=123456;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;";
-
+        //public static string ConnectionString = "Server=localhost;Database=BreadApp;User Id=sa;Password=123456;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;";
+        public static string ConnectionString = "Server=tcp:breadapp-api-server.database.windows.net,1433;Initial Catalog=BreadAppDB;Persist Security Info=False;User ID=BreadAppAdmin;Password=BreadApp@2026;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
